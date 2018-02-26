6f1ee357ae39edb93e8b8261dc9dead4191946e5
ellen102 <eavhove@gmail.com>
2018-02-26 15:46:21 +0100

    clean up first test #11

--- test/textToXML.test.js
+++ test/textToXML.test.js
@@ -1,34 +1,25 @@
 /**
- * Summary. (use period)
+ * tests for generating xml from text.
  *
- * Description. (use period)
  *
- * @file   This files defines the MyClass class.
- * @author Ellen Vanhove.
- */
-
-/**
- * Summary. (use period)
- *
- * Description. (use period)
+ * setup: https://mochajs.org/#getting-started and https://x-team.com/blog/setting-up-javascript-testing-tools-for-es6/.
+ * Based on http://chaijs.com/plugins/chai-string/
+ * equalIgnoreSpaces: ignores spaces and newlines
  *
  * @file   This files defines the MyClass class.
  * @author Ellen Vanhove.
  */
 import parseTextToXML from './../parser/parserUtils.js'
-var chai = require('chai');
+let chai = require('chai');
 chai.use(require('chai-string'));
-var assert = require('chai').assert;
+let assert = require('chai').assert;
 
-//const assert = require('assert');
-
-describe('scripts', function() {
+describe('scripts_pen', function() {
     describe('pen up', function() {
         it('should return valid xml', function() {
             let parsed = parseTextToXML('pen up');
-            //console.log(parsed)
             let expected = '<xml xmlns="http://www.w3.org/1999/xhtml">' +
-                '  <variables/>' +
+                '  <variables/>\n' +
                 '  <block id="0" type="pen_penup" x="10" y="10">' +
                 '    <next/>' +
                 '  </block>' +
