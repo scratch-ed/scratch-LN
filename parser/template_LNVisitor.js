f3be552bfda89f6555ca6e44d0f0a255a0103669
ellen102 <eavhove@gmail.com>
2018-07-05 23:58:14 +0200

    Update template visitor

--- parser/template_LNVisitor.js
+++ parser/template_LNVisitor.js
@@ -3,7 +3,7 @@
  *
  * This file consist of a template for the visitor .
  *
- * @file   This files defines the LNVisitorTemplate class.
+ * @file   This files defines the LNVisitor class.
  * @author Ellen Vanhove.
  */
 
@@ -16,10 +16,20 @@ let StringLiteral = lntokens.StringLiteral;
 let ChoiceLiteral = lntokens.ChoiceLiteral;
 import {lnparser} from "./LNParser"
 
-//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
 const BaseCstVisitor = lnparser.getBaseCstVisitorConstructor();
 
-export class InformationVisitor extends BaseCstVisitor {
+/*
+    No dispatching necessary if nothing special happens, specifically block and composite
+    only for no return values
+    docs: This base visitor includes a default implementation for all visit methods which simply invokes this.visit on all none terminals in the CSTNode's children.
+
+*/
+//const BaseCstVisitorWithDefaults = lnparser.getBaseCstVisitorConstructorWithDefaults();
+
+//DO NOT FORGET THE EXPORT
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+export class LNVisitor extends BaseCstVisitor {
 
     constructor() {
         super();
@@ -43,11 +53,33 @@ export class InformationVisitor extends BaseCstVisitor {
 
     }
 
+    block$atomic(ctx) {
+
+    }
+
+    block$composite(ctx) {
+
+    }
     atomic(ctx) {
 
     }
 
     composite(ctx) {
+
+    }
+    composite$ifelse(ctx) {
+
+    }
+
+    composite$forever(ctx) {
+
+    }
+
+    composite$repeat(ctx) {
+
+    }
+
+    composite$repeatuntil(ctx) {
 
     }
 
