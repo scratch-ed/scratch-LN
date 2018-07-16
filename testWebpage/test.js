e45e331fc907739974f995eeed85e10be04cc898
ellen102 <eavhove@gmail.com>
2018-07-16 17:20:12 +0200

    change test.html

--- testWebpage/test.js
+++ testWebpage/test.js
@@ -10,6 +10,39 @@ let workspaceCounter =0;
 
 $(document).ready(function() {
     makeTable(['say "hello"','move {10} steps','x {', 'x |'], "a test");
+    makeTable(("move {10} steps;" +
+        "turn cw {15} degrees;" +
+        "turn ccw {15} degrees;" +
+        "point in direction {90};" +
+        "point towards [mouse-pointer];" +
+        "go to x: {0} y: {0};" +
+        "go to [mouse-pointer];" +
+        "glide {1} secs to x: {0} y: {0};" +
+        "glide {1} secs to [mouse-pointer];" +
+        "change x by {10};" +
+        "set x to {0};" +
+        "change y by {10};" +
+        "set y to {0};" +
+        "set rotation style [left-right];" +
+        "if on edge, bounce;" +
+        "x position;" +
+        "y position;" +
+        "direction;").split(";"), "Motion");
+    makeTable(("show;" +
+        "hide;" +
+        "switch costume to [costume 1];" +
+        "next costume;" +
+        "next backdrop;" +
+        "switch backdrop to [backdrop];" +
+        "switch backdrop to [backdrop] and wait;" +
+        "change [color] effect by {10};" +
+        "set [color] effect to {10};" +
+        "change size by {10};" +
+        "go to [back];" +
+        "go [forward] {1} layers;" +
+        "costume [number];" +
+        "backdrop [number];" +
+        "size").split(";"), "Looks");
     //scratchify('scratch',true);
     //scroll down #makeUrLifeEasy
     //window.scrollTo(0,document.body.scrollHeight);
