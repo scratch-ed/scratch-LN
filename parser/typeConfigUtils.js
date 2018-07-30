bc5d6a3e3243e3e17ce92c60fc0c9ce05b0a3fab
ellen102 <eavhove@gmail.com>
2018-07-30 11:07:17 +0200

    verifyinput + test

--- parser/typeConfigUtils.js
+++ parser/typeConfigUtils.js
@@ -18,10 +18,57 @@ export const stringInputTypeMap = {
     "colour_picker":INPUTTYPE.DROPDOWN
 };
 
+/**
+ * maps a string of an inputtype to an inputtype
+ * @param text string textual version of inputtype
+ * @returns {*}
+ */
 export function stringToinputType(text){
     if(stringInputTypeMap[text]){
         return stringInputTypeMap[text];
     }else{
         return INPUTTYPE.NONE;
+    }
+}
+
+
+
+
+/**
+ * returns whether a certain text matches an certain inputtype
+ * @param text an text
+ * @param type an inputType
+ * @returns {boolean}
+ */
+export function verifyInputType(text,type){
+    switch (type){
+            case INPUTTYPE.NONE:
+                return false;
+            case INPUTTYPE.BOOLEAN:
+                return false;
+            case INPUTTYPE.TEXT:
+                return true;
+            case INPUTTYPE.NUMBER:
+            case INPUTTYPE.ANGLE:
+                return !/[^0-9.]/i.test(text);
+            case INPUTTYPE.INTEGER:
+                return !/\./i.test(text);
+            case INPUTTYPE.WHOLE_NUMBER:
+                if(/\./i.test(text)){
+                    return false;
+                }
+                //fallthrough
+            case INPUTTYPE.POSITIVE_NUMBER:
+                let num;
+                try{
+                    num=parseInt(text);
+                }catch(e){
+                    return false;
+                }
+                return num>=0;
+            case INPUTTYPE.COLOR:
+                return /#[0-F]{6}/i.test(text);
+            case INPUTTYPE.DROPDOWN:
+                return false;
     }
 }
